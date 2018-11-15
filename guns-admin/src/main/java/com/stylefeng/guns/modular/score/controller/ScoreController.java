915a894d90ba9148d4cc05a52296b9f296f87ebd
maguohao2018 <guohao.ma@fit2cloud.com>
2018-11-15 20:47:53 +0800

    submit

--- guns-admin/src/main/java/com/stylefeng/guns/modular/score/controller/ScoreController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/score/controller/ScoreController.java
@@ -2,64 +2,69 @@ package com.stylefeng.guns.modular.score.controller;
 
 import com.stylefeng.guns.core.base.controller.BaseController;
 import com.stylefeng.guns.core.shiro.ShiroKit;
-import com.stylefeng.guns.modular.classNo.service.IClassNoService;
+import com.stylefeng.guns.modular.score.service.IScoreService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
- * 打卡控制器
+ * 控制器
  *
  * @author maguohao
  * @Date 2018年11月17日20:27:22
  */
 @Controller
-@RequestMapping("/classNo")
+@RequestMapping("/score")
 public class ScoreController extends BaseController {
 
-    private String PREFIX = "/classNo/";
+    private String PREFIX = "/score/";
 
     @Resource
-    private IClassNoService iclassNoService;
-    /**
-     * 跳转员工打卡页面
-     */
-    @RequestMapping("/check_time_staff")
-    public String check_time_staff() {
-        return PREFIX + "check_time_staff.html";
-    }
-    /**
-     * 跳转员工历史打卡信息页面
-     */
-    @RequestMapping("/check_history_staff")
-    public String check_history_staff() {
-        return PREFIX + "check_history_staff.html";
+    private IScoreService iScoreService;
+
+    @RequestMapping("")
+    public String index() {
+        return PREFIX + "score.html";
     }
 
-    /**
-     * 跳转经理打卡页面
-     */
-    @RequestMapping("/check_time_boss")
-    public String check_time_boss() {
-        return PREFIX + "check_time_boss.html";
+    @RequestMapping("/score_add")
+    public String score_add() {
+        return PREFIX + "score_add.html";
+    }
+
+    @RequestMapping("/score_edit")
+    public String score_edit() {
+        return PREFIX + "score_edit.html";
     }
 
 
-
-    /**
-     * 新增员工打卡记录
-     */
-    @RequestMapping(value = "/insertInfo")
+    @RequestMapping(value = "/getScoreList")
     @ResponseBody
-    public Integer insertInfo() {
+    public List<Map<String, Object>> getScoreList() {
 
-        Integer userid = ShiroKit.getUser().getId();
-        Integer status = 1;
+        List<Map<String, Object>> getScoreList = null;
+        try {
+            getScoreList = this.iScoreService.getScoreList(ShiroKit.getUser().getId());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return getScoreList;
+    }
+
+    @RequestMapping(value = "/insertScore")
+    @ResponseBody
+    public Integer insertScore(@RequestBody Map map) {
+
         Integer no = 0;
         try {
-            no = this.iclassNoService.insertInfo(userid,status);
+            no = this.iScoreService.insertScore(map);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -67,18 +72,27 @@ public class ScoreController extends BaseController {
         return no;
     }
 
-
-    /**
-     * 经理审批打卡记录
-     */
-    @RequestMapping(value = "/update_boss/{id}")
+    @RequestMapping(value = "/updateScore")
     @ResponseBody
-    public Integer update(Integer id) {
+    public Integer updateScore(@RequestBody Map map) {
 
-        Integer status = 10;
         Integer no = 0;
         try {
-            no = this.iclassNoService.update(id,status);
+            no = this.iScoreService.updateScore(map);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return no;
+    }
+
+    @RequestMapping(value = "/deleteScore/{id}")
+    @ResponseBody
+    public Integer deleteScore(@PathVariable Integer id) {
+
+        Integer no = 0;
+        try {
+            no = this.iScoreService.deleteScore(id);
         }catch (Exception e){
             e.printStackTrace();
         }
--- guns-admin/src/main/java/com/stylefeng/guns/modular/score/dao/ScoreDao.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/score/dao/ScoreDao.java
@@ -7,35 +7,21 @@ import java.util.Map;
 
 /**
  * <p>
- * 打卡
+ *
  * </p>
  *
  * @author maguohao
- * @since 2018-08-19
+ * @since 2018-11-19
  */
 public interface ScoreDao {
 
 
-    /**
-     * 获取员工历史打卡信息列表
-     */
-    List<Map<String, Object>> queryStaffCheckTimeList(@Param("userid") Integer userid, @Param("flag") String flag);
+    List<Map<String, Object>> getScoreList(Integer folderId);
 
-    /**
-     * 新增员工打卡记录
-     */
-    Integer insertInfo(@Param("userid") Integer userid, @Param("status") Integer status);
+    Integer insertScore(Map map);
 
-    /**
-     * 获取经理打卡信息列表（获取所有员打卡记录）
-     */
-    List<Map<String, Object>> queryBossCheckTimeList(@Param("userid") Integer userid, @Param("flag") String flag);
+    Integer updateScore(Map map);
 
-    /**
-     * 经理审批打卡记录
-     */
-    Integer update(@Param("id") Integer id, @Param("status") Integer status);
-
-
+    Integer deleteScore(Integer id);
 
 }
