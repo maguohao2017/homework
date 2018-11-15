f4a726ba8961c00a5e2052d3665b69fc0b1083b4
mgh <402847591@qq.com>
2018-11-16 07:32:43 +0800

    班级模块

--- guns-admin/src/main/webapp/static/modular/class/class_student.js
+++ guns-admin/src/main/webapp/static/modular/class/class_student.js
@@ -1,19 +1,25 @@
 
 $(function () {
 
-    var ajax = new $ax(Feng.ctxPath + "/calss/getClassMap", function (data) {
-        Feng.success("查询成功!");
+    var ajax = new $ax(Feng.ctxPath + "/class/getClassMap", function (data) {
         console.log(data);
         $("input").each(function(){
             var key = $(this).attr("id");
             var value = data[key];
-            if(!!value){
+            if(!value){
                 value = "暂无班级信息";
             }
             $(this).val(value);
         });
     }, function (data) {
-        Feng.error("查询失败!");
+        $("input").each(function(){
+            var key = $(this).attr("id");
+            var value = data[key];
+            if(!value){
+                value = "暂无班级信息";
+            }
+            $(this).val(value);
+        });
     });
     ajax.start();
 
